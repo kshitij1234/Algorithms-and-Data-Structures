d181fc4140dde72c0afadfe526f2816c978f9491
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-07-03 18:18:18 +0530

    insertion sort

---
+++ src/Algorithms/InsertionSort.java
@@ -0,0 +1,52 @@
+/*
+ * Time - O(n^2)
+ * In place sorting
+ */
+
+
+package Algorithms;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+	
+	
+	public static void insertionSort(int ar[])
+	{
+		for(int i=1;i<ar.length;i++)
+		{
+			int temp = ar[i];
+			int hole = i;
+			
+			while(hole>0&&ar[hole-1]>temp)
+			{
+				ar[hole] = ar[hole-1];
+				hole--;
+			}
+			
+			ar[hole] = temp;
+		}
+	}
+
+
+	public static void main(String arg[])
+	{
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		int ar[] =new int[n];
+		for(int i=0;i<n;i++)
+			ar[i]=scanner.nextInt();
+		
+		insertionSort(ar);
+		
+		for(int i:ar)
+		{
+			System.out.print(i+" ");
+		}
+		
+		scanner.close();
+	}
+	
+
+}
